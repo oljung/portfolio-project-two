6cfeb76ec66c0b65490b3cba3b1df7d1dc2230e7
oljung <patrik_osterljung@hotmail.com>
2021-08-24 11:41:20 +0000

    feat: create displayContent function for displaying tab content on index page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,21 @@
-console.log('connected');
+function displayContent(event, tabName){
+    //declare variables
+    let i, tabcontent, tablinks;
+
+    // Hide all the content currently visible
+    tabcontent = document.getElementsByClassName('tabcontent');
+    for(i = 0; i < tabcontent.length; i++){
+        tabcontent[i].style.display = 'none';
+    }
+
+    // Remove active class from tab
+    tablinks = document.getElementsByClassName('tablinks');
+    for(i = 0; i < tablinks.length; i++){
+        tablinks[i].className = tablinks[i].className.replace('active', '');
+    }
+
+    // Show the tab that was clicked
+
+    document.getElementById(tabName).style.display = 'inline-block';
+    event.currentTartget.className += ' active';
+}
