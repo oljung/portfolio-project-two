e7bf15bdb2032f7cf75fa58a070223e9089cd27a
oljung <patrik_osterljung@hotmail.com>
2021-08-28 09:33:54 +0000

    feat: add functionality for changing room and displaying correct text

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,10 +7,8 @@ let speed = 100;
 
 // Wait for DOM content to load before running code
 document.addEventListener("DOMContentLoaded", function() {
-    //set default room and add active to classlist
-    let defaultRoom = document.getElementById('living-room');
-    defaultRoom.style.display = 'block';
-    defaultRoom.className += ' active';
+    //set default room
+    roomChange('living-room');
     
     //decide the text to print
     setNarratorText();
@@ -33,24 +31,28 @@ document.addEventListener("DOMContentLoaded", function() {
  * This function is called whenever the game needs to change the text in the narrator textbox
  */
 function displayNarratorText(){
+    //reset the character counter
+    index = 0;
+    
+    //clear the narrator text box for new input
+    document.getElementById('narrator').innerHTML = '';
+    
     //get the checkbox to check its value
     let checkbox = document.getElementById('auto-complete');
-    textTyper();
     
     //determine status if checkbox
-    /*if(checkbox.checked === true){
+    if(checkbox.checked === true){
         fillNarratorText();
     } else {
         textTyper();
-    }*/
+    }
 }
 
 /**
  * This function is called to set narrator text based on the current room, and what has happened in said room
  */
 function setNarratorText(){
-    index = 0;
-    let room = document.getElementsByClassName('active')[0];
+    let room = document.getElementsByClassName('active-room')[0];
     if(room.id === 'living-room'){
         txt = 'You are standing in the living room...';
     } else if(room.id === 'kitchen') {
@@ -77,7 +79,9 @@ function addText() {
  * Function for instantly filling hte narrator text box
  */
 function fillNarratorText() {
-    index = -1;
+    //stops the texttyper being called
+    clearInterval(timer);
+    //fills box with txt string
     document.getElementById('narrator').innerHTML = txt;
 }
 
@@ -91,10 +95,17 @@ function readInput(){
     //get the input from target
     let target = document.getElementById('target');
     
+    //change all digits to lower case, in case user used capital letters somewhere
+    let actionLower = action.value.toLowerCase();
+    
     //determine what to do based on action value
-    switch(action.value) {
+    switch(actionLower) {
         case 'move':
-            changeLocation(targer.value);
+            changeLocation(target.value);
+            break;
+        default:
+            txt = 'That is not a valid action';
+            displayNarratorText();
     }
 }
 
@@ -102,5 +113,47 @@ function readInput(){
  * Function used to handling the move action
  */
 function changeLocation(room) {
-    
+    //test the value of room and sends user to the correct room, or displays error message
+    switch(room) {
+        case 'living room':
+            roomChange('living-room');
+            break;
+        case 'kitchen':
+            roomChange(room);
+            break;
+        default:
+            txt = 'That is not a valid location';
+            displayNarratorText();
+    }
+}
+
+/**
+ * This function will change the main image and update the locations window with the currently active room then set and display text
+ */
+function roomChange(targetRoom) {
+    //change display of all rooms to none and remove active room
+    let rooms = document.getElementsByClassName('main-image');
+    for(let i = 0; i < rooms.length; i++) {
+        rooms[i].style.display = 'none';
+        rooms[i].className = rooms[i].className.replace('active-room', '');
+    }
+
+    //remove "active" from the location in the locations text box
+    let locations = document.getElementsByClassName('room');
+    for(let i = 0; i < locations.length; i++) {
+        locations[i].className = locations[i].className.replace('active-location', '');
+    }
+
+    //display the correct room and give it active-room class
+    let locationImage = document.getElementById(targetRoom);
+    locationImage.style.display = 'block';
+    locationImage.className += ' active-room';
+
+    //change the active location in the text box
+    let activeLocation = document.getElementsByClassName(targetRoom)[0];
+    activeLocation.className += ' active-location';
+
+    //set text and display it
+    setNarratorText();
+    displayNarratorText();
 }
